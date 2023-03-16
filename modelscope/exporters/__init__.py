7dff64c7d2ab3d726537ce724725a61140f67c5f
zhangzhenyu47 <zhangzhenyu47@jd.com>
2023-03-16 17:19:40 +0800

    fix the bug that the model only save 1/wordl-size weights

---
+++ modelscope/exporters/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+
+from modelscope.utils.import_utils import is_tf_available, is_torch_available
+from .base import Exporter
+from .builder import build_exporter
+
+if is_tf_available():
+    from .nlp import CsanmtForTranslationExporter
+    from .tf_model_exporter import TfModelExporter
+if is_torch_available():
+    from .nlp import SbertForSequenceClassificationExporter, SbertForZeroShotClassificationExporter
+    from .torch_model_exporter import TorchModelExporter
